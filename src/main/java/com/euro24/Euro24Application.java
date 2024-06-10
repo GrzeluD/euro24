776765a53ca0bec345e07cf600569676bd803a0b
GrzeluD <grzeluud@gmail.com>
2024-06-10 16:14:37 +0200

    euro24 project init

---
+++ src/main/java/com/euro24/Euro24Application.java
@@ -0,0 +1,13 @@
+package com.euro24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Euro24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Euro24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/euro24/Euro24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euro24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Euro24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
